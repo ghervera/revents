fa9a767aa4ea0004a85f5863f2d12706343ebe78
Gian Hervera <gianh@gmail.com>
2020-01-22 10:19:44 -0600

    End of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,20 +2,39 @@ import React, {Component, Fragment} from 'react';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/event/home/HomePage';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/event/user/PeopleDashboard/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/event/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/event/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+
+
 
 
 
 class App extends Component {
   render() {
     return (
-      
       <Fragment>
-        <NavBar/>
-        <Container className="main">
-          <EventDashboard/>
-        </Container>    
-      </Fragment>
-      
+        <Route exact path='/' component={HomePage} />
+        <Route 
+        path='/(.+)' 
+        render={() => (
+          <Fragment>
+          <NavBar/>
+          <Container className="main">
+              <Route path='/event' component={EventDashboard} />
+              <Route path='/event/:id' component={EventDetailedPage} />
+              <Route path='/people' component={PeopleDashboard} />
+              <Route path='/profile/:id' component={UserDetailedPage} />
+              <Route path='/settings' component={SettingsDashboard} />
+              <Route path='/createEvent' component={EventForm} />
+          </Container>    
+        </Fragment>
+        )}/>
+      </Fragment>  
     );
   }
 }
